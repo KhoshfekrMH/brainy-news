f1265d9188bb93cae67cd081feded06205ceb5bd
KhoshfekrMH <Khoshfekr2001@protonmail.com>
2024-12-16 23:50:54 +0330

    add user context and some changes in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Button from "@mui/material/Button";
-
 import "./App.css";
 
-function Home() {
-  return <h1>Home</h1>;
-}
-
-function About() {
-  return <h1>About</h1>;
-}
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-      <Button variant="contained" color="primary">
-        Navigate
-      </Button>
-    </Router>
-  );
-}
+const App = () => {
+  return <h1>HelloWorld!</h1>;
+};
 
 export default App;
