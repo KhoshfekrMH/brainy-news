4375d5ac85344ff7c8af83d13a8a7839838958ac
KhoshfekrMH <Khoshfekr2001@protonmail.com>
2024-12-24 02:16:05 +0330

    style(Hompage.js-pages): change style of NewsCard list

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Grid from "@mui/material/Grid";
 import PageLayout from "../shared/layout/PageLayout";
 import NewsCard from "../shared/components/UIElements/NewsCard";
 import { newsItems, banners } from "../shared/data/DummyData";
@@ -12,15 +13,19 @@ const Homepage = () => {
   };
   return (
     <PageLayout showBanner={true} banners={banners}>
-      {newsItems.map((news) => (
-        <NewsCard
-          key={news.id}
-          image={news.image}
-          title={news.title}
-          intro={news.intro}
-          onReadMore={handleReadMore}
-        />
-      ))}
+      <Grid container spacing={2} justifyContent="center">
+        {newsItems.map((news) => (
+          <Grid item xs={12} sm={6} md={4}>
+            <NewsCard
+              key={news.id}
+              image={news.image}
+              title={news.title}
+              intro={news.intro}
+              onReadMore={handleReadMore}
+            />
+          </Grid>
+        ))}
+      </Grid>
     </PageLayout>
   );
 };
