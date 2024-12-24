f485c00abada2bb6b0b97e10db6db2a15f031750
KhoshfekrMH <Khoshfekr2001@protonmail.com>
2024-12-25 01:56:25 +0330

    feat(News-page): add News.js to App.js
    Change some context and Put New.js in App.js

--- frontend/src/pages/News.js
+++ frontend/src/pages/News.js
@@ -1,28 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import Grid from "@mui/material/Grid";
 import PageLayout from "../shared/layout/PageLayout";
 import NewsCard from "../shared/components/UIElements/NewsCard";
-import { banners } from "../shared/data/DummyData";
 import { NewsContext } from "../shared/context/NewsContext";
 
-const Homepage = () => {
-  const { newsItems } = React.useContext(NewsContext);
+const News = () => {
+  const { newsItems } = useContext(NewsContext);
+
   const handleReadMore = () => {
-    {
-      /*TODO: remove this dummy for test*/
-    }
     console.log("Read more clicked");
   };
 
-  const recentNews = newsItems.slice(0, 3);
-
   return (
-    <PageLayout showBanner={true} banners={banners}>
+    <PageLayout showBanner={false}>
       <Grid container spacing={2} justifyContent="center">
-        {recentNews.map((news) => (
-          <Grid item xs={12} sm={6} md={4}>
+        {newsItems.map((news) => (
+          <Grid item key={news.id} xs={12} sm={6} md={4}>
             <NewsCard
-              key={news.id}
               image={news.image}
               title={news.title}
               intro={news.intro}
@@ -35,4 +29,4 @@ const Homepage = () => {
   );
 };
 
-export default Homepage;
+export default News;
